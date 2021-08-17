f2c65a411902d4e520e75254843d7c10f744d34b
Vaibhav Jain <vaibhavjain2418@gmail.com>
2021-08-18 00:20:29 +0530

    database connectivity established

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 from flask import Flask, render_template,request
+from flask_pymongo import PyMongo
+from pymongo import message
 
 app = Flask(__name__)
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/schedulardb")
+db = mongodb_client.db
+
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
-@app.route('/calendarPage',methods=["GET","POST"])
+@app.route('/calendarPage')
 def calendar_page():
-   if request.method== "POST":
-         x=request.form
-         y=x['date']
-         print(y)
-         Message={"Message":"problem solved"}
-         return Message
+  
+   return render_template('calendar_page.html')
 
-   a = 'hello'
-   return render_template('calendar_page.html', a = a)
+@app.route('/getdata', methods=['GET','POST'])
+def data_get():
+   if request.method == "POST":
+         requestInput = request.form
+         eventDate = '2021-07-14'
+         eventName = requestInput.get('name')
+         eventDes = requestInput.get('content1')
+         eventType = requestInput.get('optradio')
+         eventSt = requestInput.get('stTime')
+         eventEnd = requestInput.get('endTime')
+         if(len(eventName)>0):
+
+            db.schedulardb.insert_one({'Start':eventDate, 'title':eventName })
+            print("insert running")
+            
+            e1 = list(db.schedulardb.find())
+            print(e1)
+            print(type(e1))
+            e2 = [{k: v for k, v in d.items() if k != '_id'} for d in e1] #remove the _id key-value from extracted document
+            print(e2)
+
+   return render_template('/calendar_page.html',events = e2)
+
+
 
 # @app.route('/getdata/<dt>', methods=['GET','POST'])
 # def data_get(dt):
